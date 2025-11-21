72092e1b603165da2c7f0ac253574c5618b44481
CaptainChicky <70708832+CaptainChicky@users.noreply.github.com>
2025-11-21 01:56:21 -0500

    ef

--- Source/Main.hx
+++ Source/Main.hx
@@ -122,7 +122,7 @@ class Main extends Sprite {
 			enemyManager.clearAllEnemies();
 
 			// Start the level!
-			levelManager.loadLevel("assets/levels/level1.json");
+			levelManager.loadLevel("assets/levels/level2.json");
 		}
 	}
 
